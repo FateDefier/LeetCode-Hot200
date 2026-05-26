3264e6c7cfe4dd8cae3a840c55f373831050064c
xiu-yuan-siu <yuanxiu_0903@qq.com>
2026-05-26 12:06:37 +0800

    13-15

--- 1. Beginner-Level/15. 最小栈.cpp
+++ 1. Beginner-Level/15. 最小栈.cpp
@@ -40,6 +40,7 @@ public:
     // 自动析构
 };
 
+// stack
 class MinStack {
 private:
     stack<int> s;
@@ -65,6 +66,55 @@ public:
 
     int getMin() {
         return mins.top();
+    }
+
+    // 自动析构
+};
+
+// 最优版本：但较难理解 -- 差值栈+int min
+class MinStack {
+private:
+    stack<long long> diffstack;         // 用 long long 防止 int 减法溢出(如 INT_MAX - (-1)) -- 存 val - 当前最小值的 差值
+    long long min;                      // 当前最小值
+public:
+    MinStack() {            
+        
+    }
+
+    void push(int val) {
+        long long v = val;              // 类型转换
+        if (diffstack.empty()) {
+            min = v;
+            diffstack.push(0LL);        // 第一个元素差值为 0(long long类型)
+        } else {
+            diffstack.push(v - min);
+            if (v < min) {
+                min = v;
+            }
+        }
+    }
+
+    void pop() {
+        long long diff = diffstack.top();           // diff = val(当前最小) - min(上一个最小)
+        diffstack.pop();
+        if (diff < 0) {
+            // 如果栈顶元素即是“创造了新低”的元素(存差值)，则出栈需更新min到上一个最小值
+            min = min - diff;
+        }
+        // diff > 0 则说明栈顶元素不是“创造了新低”的元素
+    }
+
+    int top() {
+        long long diff = diffstack.top();
+        if (diff < 0) {
+            return (int)min;            // 注意类型转换
+        } else {
+            return (int)(min + diff);
+        }
+    }
+
+    int getMin() {
+        return min;
     }
 
     // 自动析构
